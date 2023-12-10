608d8b2725f62828b75a344a6a88d7681487189f
Angeles Kiev <89122274+Angeles-Orta@users.noreply.github.com>
2023-12-09 23:41:53 -0600

    Update test_main.py
    test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,12 @@
 import unittest
-import os #
-import tempfile#
 from unittest.mock import patch
 from io import StringIO
 from main import parse_args
 from password import derive_key
-from polynomial import encrypt_file
-from cryptography.fernet import Fernet #
 
 class MainTestCase(unittest.TestCase):
     def test_parse_args_c_encrypt(self):
+        # Caso de prueba con linea de comandos de opcion c correcta
         with patch('sys.argv', ['main.py', 'c', '-p', 'pairs.txt', '-n', '10', '-m', '5', '-f', 'pruebaTest.txt']):
             args = parse_args()
         self.assertEqual(args.option, 'c')
@@ -19,6 +16,7 @@ class MainTestCase(unittest.TestCase):
         self.assertEqual(args.clear_file, 'pruebaTest.txt')
 
     def test_parse_args_d_decrypt(self):
+        # Caso de prueba con linea de comandos de opcion d correcta
         with patch('sys.argv', ['main.py', 'd', '-p', 'pairs.txt', '-f', 'pruebaTest.aes']):
             args = parse_args()
         self.assertEqual(args.option, 'd')
@@ -26,6 +24,7 @@ class MainTestCase(unittest.TestCase):
         self.assertEqual(args.clear_file, 'pruebaTest.aes')
 
     def test_parse_args_invalid_option(self):
+        # Caso de prueba con linea de comandos incorrecta
         with patch('sys.argv', ['main.py', 'x']):
             with patch('sys.stderr', new_callable=StringIO) as mock_stderr:
                 with self.assertRaises(SystemExit) as cm:
@@ -34,43 +33,18 @@ class MainTestCase(unittest.TestCase):
                 self.assertIn('invalid choice', mock_stderr.getvalue().lower())
 
     def test_derive_key(self):
-            # Caso de prueba con una contraseña conocida
-            password = "mi_contraseña_123"
-            expected_key = b'l3RMFUtpa9Tp-auqDRrl54wKhYUMz6fNX3YiVrWcqtk='  # Este valor puede variar si cambias la contraseña
-            # Llamada a la función
-            actual_key = derive_key(password)
-            # Verificación de la igualdad
-            self.assertEqual(actual_key, expected_key)
+        # Caso de prueba con una contraseña conocida
+        password = "mi_contraseña_123"
+        expected_key = b'l3RMFUtpa9Tp-auqDRrl54wKhYUMz6fNX3YiVrWcqtk=' 
+        actual_key = derive_key(password)
+        self.assertEqual(actual_key, expected_key)
 
     def test_derive_key_different_password(self):
         # Caso de prueba con otra contraseña conocida
         password = "otra_contraseña_456"
-        expected_key = b'04Ygta5ZSToJ-E8cK74WCkpFr-vwTJUiuahZ0ZOGyKs='  # Este valor puede variar si cambias la contraseña
-        # Llamada a la función
+        expected_key = b'04Ygta5ZSToJ-E8cK74WCkpFr-vwTJUiuahZ0ZOGyKs='
         actual_key = derive_key(password)
-        # Verificación de la igualdad
         self.assertEqual(actual_key, expected_key)
 
-    def test_encrypt_file(self):
-            # Crear un archivo temporal para usar como clear_file
-            with tempfile.NamedTemporaryFile(delete=False) as clear_temp_file:
-                clear_temp_file_name = clear_temp_file.name
-            try:
-                # Escribir datos en el archivo temporal
-                clear_temp_file.write(b"Datos de prueba")
-                clear_temp_file.flush()
-                # Crear una instancia de Polynomial
-                your_instance = Polynomial(pair_file='pairs.txt')
-                # Llamar a la función encrypt_file
-                your_instance.encrypt_file(clear_temp_file_name, password='test_password')
-                # Verificar que el archivo cifrado ('.aes') se haya creado correctamente
-                encrypted_file_path = clear_temp_file_name + '.aes'
-                self.assertTrue(os.path.exists(encrypted_file_path))
-            finally:
-                # Eliminar los archivos temporales después de las pruebas
-                os.remove(clear_temp_file_name)
-                os.remove(encrypted_file_path)
-
-            
-if __name__ == '__main__':
+if _name_ == '_main_':
     unittest.main()
